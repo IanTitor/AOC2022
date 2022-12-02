a6c7ac46e860b01eeb4709bab8c40b8ef6e2bcdc
Ian Titor <iantitor@protonmail.com>
2022-12-02 12:07:06 -0500

    day 1 part 1 done

---
+++ day 1/part1.py
@@ -0,0 +1,11 @@
+f = open('input.txt', 'r')
+cals = f.read()
+f.close()
+cals = cals.replace('\n\n', '], [').replace('\n', ', ')
+cals = eval('[[' + cals + ']]')
+greatest = 0
+for elf in cals:
+    elfCal = sum(elf)
+    if elfCal > greatest:
+        greatest = elfCal
+print('greatest cal is:', greatest)
