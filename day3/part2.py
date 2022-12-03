63c6285143cb57c5488a7ae5120541ba009b2083
IanTitor <iantitor@protonmail.com>
2022-12-03 13:40:56 -0500

    done day 3 part 2, and changed part 1 a little

--- day3/part2.py
+++ day3/part2.py
@@ -3,8 +3,7 @@ contents = f.read()[:-1]
 f.close()
 contents = contents.split('\n')
 prioritySum = 0
-for content in contents:
-    half = int(len(content)/2)
-    sameItem = list(set(content[:half]).intersection(set(content[half:])))[0]
+for c in range(0, len(contents), 3):
+    sameItem = list(set(contents[c]).intersection(set(contents[c + 1])).intersection(set(contents[c + 2])))[0]
     prioritySum += (ord(sameItem) - 38) % 58
 print('sum of common item priorities is:', prioritySum)
