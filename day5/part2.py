7acd5dc54072158b4d841dbd6a5e4d190d2f4b11
IanTitor <iantitor@protonmail.com>
2022-12-05 14:07:23 -0500

    done day 5 part 2

---
+++ day5/part2.py
@@ -0,0 +1,21 @@
+f = open('input.txt', 'r')
+info = f.read()[:-1]
+f.close()
+info = info.split('\n')
+numOfStacks = int((len(info[0]) + 1) / 4)
+cargo = [[] for x in range(numOfStacks)]
+row = 0
+while info[row][0] != ' ':
+    for col in range(numOfStacks):
+        if info[row][col * 4 + 1] != ' ':
+            cargo[col] = [info[row][col * 4 + 1]] + cargo[col]
+    row += 1
+operations = info[row + 2:]
+for operation in operations:
+    amount, origin, destination = eval(operation.replace('move ', '').replace(' from', ', ').replace(' to', ', '))
+    cargo[destination - 1] += cargo[origin - 1][-amount:]
+    cargo[origin - 1] = cargo[origin - 1][:-amount]
+topCrates = ''
+for col in cargo:
+    topCrates += col[-1]
+print('top crates are:', topCrates)
